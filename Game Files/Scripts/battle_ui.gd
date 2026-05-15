4d14401dad44a3b0e10a826a00b527a87de2db67
QTheGuy000 <qlebovic6505@panther.chaffey.edu>
2026-05-14 19:20:51 -0700

    fixed the winning screen

--- Game Files/Scripts/battle_ui.gd
+++ Game Files/Scripts/battle_ui.gd
@@ -4,8 +4,17 @@ var P1icons = []
 var P2icons = []
 var player_icons = [Control]
 var icon_scene = preload("res://Scenes/Char_UI.tscn")
+var players_in_game = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for i in GlobalVars.active_players.size():
+		if not GlobalVars.active_players[i] == 0:
+			players_in_game.append(i)
+	print(players_in_game)
+	if GlobalVars.mode == "stock":
+		$TimeLeft.visible = false
+	else:
+		$TimeLeft.visible = true
 	for i in range(4):
 		if GlobalVars.active_players[i] > 0:
 			var icon = icon_scene.instantiate()
@@ -21,10 +30,10 @@ func _process(delta):
 
 
 func update_stock(player, stock):
-	player_icons[player].update_stock(stock)
+	player_icons[players_in_game.find(player)].update_stock(stock)
 
 func update_score(player, score):
-	player_icons[player].update_stock(score)
+	player_icons[players_in_game.find(player)].update_stock(score)
 
 func update_damage(player, damage):
-	player_icons[player].update_damage(damage)
+	player_icons[players_in_game.find(player)].update_damage(damage)
